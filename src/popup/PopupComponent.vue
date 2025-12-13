da14d5998ff9c29d0014a9652215e1fce47c231e
JustHappy <122306263+Simonmie@users.noreply.github.com>
2025-12-13 21:14:38 +0800

    Initial commit

---
+++ src/popup/PopupComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="popup-component">这是一个弹窗</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.popup-component {
+  width: 300px;
+  height: 200px;
+}
+</style>
